9ad429e3e32c94cb54c289105a978ba27ad100a6
David Buckley <david@davidbuckley.ca>
2022-12-10 00:24:32 -0500

    add day 10 solution

--- 2022/day10/day10.py
+++ 2022/day10/day10.py
@@ -1,30 +1,47 @@
 #!/usr/bin/env python
 
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
 from aoc_utils import * # type: ignore
 
 from aocd import get_data
 
 
 data = get_data(year=2022, day=10, block=True)
-print(data)
+lines = iter(data.splitlines())
 
-# submit(answer, part="a", day=10, year=2022)
+t = 0
+
+X = 1
+i = 1
+
+waiting = 0
+
+px = 0
+
+try: 
+    while True:
+        dx, dy = px%40, px // 40
+        px += 1
+        if dx == X or dx == X-1 or dx == X+1:
+            print('#', end='')
+        else:
+            print('.', end='')
+        if px % 40 == 0:
+            print()
+
+        if waiting:
+            X += waiting
+            waiting = 0
+        else:
+            line = next(lines)
+            if line.startswith("noop"):
+                pass
+            else:
+                _, waiting = line.split()
+                waiting = int(waiting)
+        i += 1
+
+        if (i + 20) % 40 == 0:
+            t += X * i
+except StopIteration:
+    pass
+    # print(t) - Part 1
