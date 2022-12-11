445e434ba7bb35ff5f55d0a7b396b83e2b5eb2e3
David Buckley <david@davidbuckley.ca>
2022-12-11 01:21:58 -0500

    add day 11 solution

--- 2022/day11/day11.py
+++ 2022/day11/day11.py
@@ -1,30 +1,56 @@
 #!/usr/bin/env python
 
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
 from aoc_utils import * # type: ignore
 
 from aocd import get_data
 
+import re
 
 data = get_data(year=2022, day=11, block=True)
-print(data)
 
-# submit(answer, part="a", day=11, year=2022)
+lines = data.splitlines()
+
+monkeys = {}
+factor = 1
+for i in range(0, len(lines), 7):
+    g = lines[i:i+7]
+    n = ints(g[0])[0]
+    items = ints(g[1])
+    op = re.search(r"Operation: new = (.*)", g[2]).group(1)
+    test = int(re.search(r"Test: divisible by (.*)", g[3]).group(1))
+    t = int(re.search(r"If true: throw to monkey (.*)", g[4]).group(1))
+    f = int(re.search(r"If false: throw to monkey (.*)", g[5]).group(1))
+
+    factor *= test
+
+    monkeys[n] = {
+        "n": n,
+        "items": items,
+        "op": op,
+        "test": test,
+        "t": t,
+        "f": f,
+        "count": 0,
+    }
+
+for round in range(10000):
+    for n in monkeys:
+        monkey = monkeys[n]
+        while monkey["items"]:
+            worry = monkey["items"][0]
+            monkey["items"] = monkey["items"][1:]
+            monkey["count"] += 1
+            test = monkey["test"]
+            t = monkey["t"]
+            f = monkey["f"]
+            old = worry
+
+            new = eval(monkey["op"])
+            # new = math.floor(new / 3)
+            if new % test == 0:
+                target = t
+            else:
+                target = f
+            monkeys[target]["items"].append(new % factor)
+
+print([(m["n"], m["count"]) for m in sorted(monkeys.values(), key=lambda m: m["count"], reverse=True)])
