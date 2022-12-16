98f6578176a38fc297155d848758b49cc2bd6be2
David Buckley <david@davidbuckley.ca>
2022-12-16 17:04:38 -0500

    cleanup day 16

--- 2022/day16/day16.py
+++ 2022/day16/day16.py
@@ -1,69 +1,52 @@
 #!/usr/bin/env python
 
+from collections import deque, namedtuple
+from dataclasses import dataclass
+from typing import List
 import re
+
 from aocd import get_data
 
 data = get_data(year=2022, day=16, block=True)
-
 d = {}
 
-from dataclasses import dataclass
-from typing import List
+Valve = namedtuple('Valve', ['rate', 'leads_to'])
 
-@dataclass
-class Valve:
-    name: str
-    rate: int
-    leads_to: List[str]
-    closed: bool = True
-    idx: int = 0
+to_sel = {}
+by_name = {}
+initial_state = 0
+for i, line in enumerate(sorted(data.splitlines())):
+    name, rate, to = re.match(r"Valve (.*) has flow rate=(\d+); tunnels? leads? to valves? (.*)", line).groups()
+    r = int(rate)
+    by_name[name] = (r, to.split(", "))
 
-    def selector(self):
-        return 1 << self.idx
-
-for line in data.splitlines():
-    m = re.match(r"Valve (.*) has flow rate=(\d+); tunnels? leads? to valves? (.*)", line)
-    name, rate, valves = m.groups()
-    d[name] = Valve(name, int(rate), valves.split(", "), int(rate) != 0)
-
-max_state = 0
-state = 0
-translation = {}
-valves = {}
-for i, name in enumerate(sorted(d.keys())):
-    d[name].idx = i
-    if not d[name].closed:
-        state |= (1 << i)
-    max_state = (max_state << 1) | 1
-    translation[name] = i
-
+    sel = 1 << i
+    to_sel[name] = sel
+    initial_state |= (r == 0) << i
 
 def sel(name):
-    return d[name].selector()
+    return to_sel[name]
 
-for key, value in d.items():
-    value.leads_to = list(map(sel, value.leads_to))
-    valves[value.selector()] = value
+valves = {}
+for name, (rate, to) in by_name.items():
+    valves[to_sel[name]] = Valve(rate, list(map(sel, to)))
 
-from collections import deque
-queue = deque()
-
-queue.appendleft((sel("AA"), sel("AA"), state, 26, 0))
-
-s = 0
+max_state = 2 ** len(valves) - 1
+max_pressure = 0
 best = {}
 
+queue = deque()
+queue.appendleft((sel("AA"), sel("AA"), initial_state, 26, 0))
+
 while queue:
-    node = queue.pop()
-    msel, esel, state, remaining, pressure = node
-    # print(msel, esel, bin(state), remaining)
+    msel, esel, state, remaining, pressure = queue.pop()
     if (msel, esel, state) in best and pressure <= best[(msel, esel, state)]:
         continue
 
     best[(msel, esel, state)] = pressure
 
-    if (remaining <= 0 or state == max_state) and pressure > s:
-        s = pressure
+    if (remaining <= 0 or state == max_state) and pressure > max_pressure:
+        max_pressure = pressure
         continue
 
     mt = None
@@ -91,4 +74,4 @@ while queue:
             for ev in valves[esel].leads_to:
                 queue.appendleft((mv, ev, state, remaining - 1, pressure))
 
-print(s)
+print(max_pressure)
