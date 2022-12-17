d4ae3b5c4dad3312e8b883f6ca0b43e8db0013c2
David Buckley <david@davidbuckley.ca>
2022-12-17 11:08:25 -0500

    add day 17 solution

--- 2022/day17/day17.py
+++ 2022/day17/day17.py
@@ -1,30 +1,88 @@
 #!/usr/bin/env python
 
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
-from aoc_utils import * # type: ignore
-
 from aocd import get_data
 
+raw = get_data(year=2022, day=17, block=True)
+data = [-1 if c == "<" else 1 for c in raw]
 
-data = get_data(year=2022, day=17, block=True)
-print(data)
+t = 0
 
-# submit(answer, part="a", day=17, year=2022)
+DASH = { (0, 2), (0, 3), (0, 4), (0, 5) }
+PLUS = { (0, 3), (1, 2), (1, 3), (1, 4), (2, 3) }
+CORNER = { (2, 4), (1, 4), (0, 2), (0, 3), (0, 4) }
+BAR = { (0, 2), (1, 2), (2, 2), (3, 2) }
+SQUARE = { (0, 2), (0, 3), (1, 2), (1, 3) }
+
+rocks = [DASH, PLUS, CORNER, BAR, SQUARE]
+
+occupied = set()
+wind_ptr = 0
+rock_ptr = 0
+
+FLOOR = 1
+
+def lift(rock, amt):
+    return set((y+amt, x) for (y, x) in rock)
+
+def shift(rock, d):
+    return set((y, x+d) for y, x in rock)
+
+def gx(p):
+    return p[1]
+
+def display(points):
+    s = ''
+    print("0123456")
+    my = max(points)[0]
+    for y in range(my+1):
+        for x in range(7):
+            if (y, x) in points:
+                s += '#'
+            else:
+                s += ' '
+        s += '\n'
+    print('\n'.join(s.splitlines()[::-1]))
+
+def collide(rock):
+    return occupied.intersection(rock)
+
+goal = 1000000000000
+tick = 0
+
+memory = {}
+while tick < goal:
+    state = (rock_ptr, wind_ptr)
+    if state in memory:
+        ot, of = memory[state]
+        if (goal - tick) % (tick - ot) == 0:
+            print(FLOOR - 1 + (goal - tick) // (tick - ot) * (FLOOR - of))
+            break
+    memory[state] = (tick, FLOOR)
+            
+    rock = lift(rocks[rock_ptr], FLOOR+3)
+    rock_ptr = (rock_ptr + 1) % len(rocks)
+
+    falling = True
+
+    while falling:
+        movement = data[wind_ptr]
+        wind_ptr = (wind_ptr + 1) % len(data)
+
+        sr = shift(rock, movement)
+        if max(sr, key=gx)[1] >= 7 or min(sr, key=gx)[1] < 0 :
+            pass
+        elif collide(sr):
+            pass
+        else:
+            rock = sr
+
+        lr = lift(rock, -1)
+        if collide(lr) or min(lr)[0] <= 0:
+            falling = False
+        else:
+            rock = lr
+    FLOOR = max(FLOOR, max(rock)[0]+1)
+    clear = 0
+    occupied.update(rock)
+
+    tick += 1
