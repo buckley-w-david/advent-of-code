f9f9225ab88917f88f4aebc27c198f77edd48d96
David Buckley <david@davidbuckley.ca>
2022-12-24 01:12:32 -0500

    add day 24 solution

--- 2022/day24/day24.py
+++ 2022/day24/day24.py
@@ -19,12 +19,142 @@
 
 # ShuntingYard
 # Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
-from aoc_utils import * # type: ignore
+# from aoc_utils import * # type: ignore
 
-from aocd import get_data
+# from aocd import get_data
+
+import enum
+
+class Direction(enum.Enum):
+    NORTH = (-1, 0)
+    EAST = (0, 1)
+    SOUTH = (1, 0)
+    WEST = (0, -1)
+    NE = (-1, 1)
+    SE = (1, 1)
+    NW = (-1, -1)
+    SW = (1, -1)
 
 
-data = get_data(year=2022, day=24, block=True)
-print(data)
+# data = get_data(year=2022, day=24, block=True)
+with open('input.txt', 'r') as f:
+    data = f.read()
+#data = """#.######
+##>>.<^<#
+##.<..<<#
+##>v.><>#
+##<^v^^>#
+#######.#"""
+lines = data.splitlines()
 
-# submit(answer, part="a", day=24, year=2022)
+tiles = {
+    '.': 0,
+    '#': 1,
+    '>': 2,
+    '<': 3,
+    '^': 4,
+    'v': 5,
+}
+field = []
+walls = set()
+blizzards = set()
+width = len(lines[0])
+height = len(lines)
+start = (0, lines[0].index('.'))
+target = (height-1, lines[-1].index('.'))
+for y, line in enumerate(lines):
+    for x, c in enumerate(line):
+        if c == '#':
+            walls.add((y, x))
+        elif c == '>':
+            blizzards.add(((y, x), Direction.EAST))
+        elif c == '<':
+            blizzards.add(((y, x), Direction.WEST))
+        elif c == '^':
+            blizzards.add(((y, x), Direction.NORTH))
+        elif c == 'v':
+            blizzards.add(((y, x), Direction.SOUTH))
+
+def bliz_pos(blizzard, g):
+    ((y, x), d) = blizzard
+    dy, dx = d.value
+    return ((y - 1 + dy*g) % (height-2)) + 1, ((x - 1 + dx*g) % (width-2)) + 1
+
+from functools import cache
+
+@cache
+def blocked(generation):
+    return frozenset( bliz_pos(b, generation) for b in blizzards )
+
+fastest = float('inf')
+def neighbours(state):
+    p, generation = state
+    if p == target or generation > fastest:
+        return []
+
+    y, x = p
+    no_go = blocked(generation)
+
+    for d in [Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST]:
+        dy, dx = d.value
+        cy, cx = y+dy, x+dx
+        candidate = (cy, cx)
+        if 0 <= cy < height and 0 <= cx < width and candidate not in walls and candidate not in no_go:
+            yield candidate, generation+1
+    if p not in no_go:
+        yield p, generation+1
+
+from collections import deque
+history = set([(start, 1)])
+queue = deque([(start, 1)])
+
+
+while queue:
+    state = queue.pop()
+    if state[0] == target and state[1] < fastest:
+        print('reached', target, 'in', state[1]-1)
+        fastest = state[1]
+
+    for other_node in neighbours(state):
+        if other_node not in history:
+            history.add(other_node)
+            queue.appendleft(other_node)
+
+start, target = target, start
+
+history = set([(start, fastest)])
+queue = deque([(start, fastest)])
+fastest = float('inf')
+
+while queue:
+    state = queue.pop()
+    if state[0] == target and state[1] < fastest:
+        print('reached', target, 'in', state[1]-1)
+        fastest = state[1]
+
+    for other_node in neighbours(state):
+        if other_node not in history:
+            # print(state[1])
+            # display(other_node[0], state[1])
+            # breakpoint()
+            history.add(other_node)
+            queue.appendleft(other_node)
+
+start, target = target, start
+history = set([(start, fastest)])
+queue = deque([(start, fastest)])
+fastest = float('inf')
+
+while queue:
+    state = queue.pop()
+    if state[0] == target and state[1] < fastest:
+        print('reached', target, 'in', state[1]-1)
+        fastest = state[1]
+
+    for other_node in neighbours(state):
+        if other_node not in history:
+            # print(state[1])
+            # display(other_node[0], state[1])
+            # breakpoint()
+            history.add(other_node)
+            queue.appendleft(other_node)
