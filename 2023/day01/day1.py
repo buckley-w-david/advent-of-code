d798c3e6e36d8ab33270bd0189582502f43ce2d2
David Buckley <david@davidbuckley.ca>
2023-12-01 18:08:44 -0500

    add day 1

--- 2023/day01/day1.py
+++ 2023/day01/day1.py
@@ -1,24 +1,32 @@
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
+import re
 
 from aoc_utils import * # type: ignore
 from aocd import get_data
 
 data = get_data(year=2023, day=1, block=True)
+
+lookup = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+}
+
+def to_i(s):
+    if s.isdigit():
+        return int(s)
+    else:
+        return lookup[s]
+
+total = 0
+for line in data.splitlines():
+    digits = re.findall(fr"(?=(\d|{'|'.join(lookup.keys())}))", line)
+    first, last = digits[0], digits[-1]
+    total += 10*to_i(first) + to_i(last)
+
+print(total)
