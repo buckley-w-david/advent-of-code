75bde3d78cc1038e829d6d119f36dca42c223e96
David Buckley <david@davidbuckley.ca>
2023-12-05 00:55:44 -0500

    add day 5 part 1 solution

--- 2023/day05/day5.py
+++ 2023/day05/day5.py
@@ -1,24 +1,47 @@
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
-
+from collections import defaultdict
+import re
 from aoc_utils import * # type: ignore
 from aocd import get_data
 
 data = get_data(year=2023, day=5, block=True)
+blocks = data.split("\n\n")
+
+seeds = ints(blocks[0])
+
+mappings = defaultdict(dict)
+
+steps = []
+for block in blocks[1:]:
+    lines = block.splitlines()
+    conversion = re.match(r"(.*)-to-(.*) map:", lines[0])
+    steps.append((conversion.group(1), conversion.group(2)))
+
+    mapping = {}
+    for line in lines[1:]:
+        dst_start, src_start, length = ints(line)
+        mapping[(src_start, src_start+length)] = (dst_start, dst_start+length)
+    mappings[conversion.group(1)][conversion.group(2)] = mapping
+
+def part_one(seeds, mappings):
+    seeds = seeds[:]
+    for i in range(len(seeds)):
+        for src, dst in steps:
+            ranges = mappings[src][dst]
+            for (ss, se), (ds, _) in ranges.items():
+                if ss <= seeds[i] < se:
+                    v =  ds + (seeds[i] - ss)
+                    seeds[i] = v
+                    break
+    return min(seeds)
+
+
+"""
+# Part 2 plan
+The numbers are too large to actually forward calculate from seeds to location
+
+Instead calculate backwards from the min location to the seeds to find what seed is needed to get that.
+It is unlikely any seed exactly hits the minimum (which is 0), so we will have to keep track of ranges.
+
+The min is either the smallest value that hits the lowest range in location, or the smallest value in an earlier
+mapping that misses a later mapping (and so carries through)
+"""
