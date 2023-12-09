7b587166c47377a4e7001d028d15e06bc607c823
David Buckley <david@davidbuckley.ca>
2023-12-09 00:16:15 -0500

    add 2023 day 9 solution

--- 2023/day09/day9.py
+++ 2023/day09/day9.py
@@ -1,24 +1,40 @@
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
-
-from aoc_utils import * # type: ignore
+from aoc_utils import *
 from aocd import get_data
 
 data = get_data(year=2023, day=9, block=True)
+
+def parse_sequences(data):
+    return [ints(line) for line in data.splitlines()]
+
+def part_one(data):
+    s = 0
+    for sequence in parse_sequences(data):
+        steps = [sequence]
+        seq = sequence
+        while not all(n == 0 for n in seq):
+            seq = [seq[i+1]-seq[i] for i in range(0, len(seq)-1)]
+            steps.append(seq)
+
+        above = 0
+        for step in reversed(steps[:-1]):
+            above = step[-1] + above
+        s += above
+    return s
+
+def part_two(data):
+    s = 0
+    for sequence in parse_sequences(data):
+        steps = [sequence]
+        seq = sequence
+        while not all(n == 0 for n in seq):
+            seq = [seq[i+1]-seq[i] for i in range(0, len(seq)-1)]
+            steps.append(seq)
+
+        above = 0
+        for step in reversed(steps[:-1]):
+            above = step[0] - above
+        s += above
+    return s
+
+print(part_one(data))
+print(part_two(data))
