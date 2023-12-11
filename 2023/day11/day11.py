19e9755b8f1c975096544636fc0d73f78940957e
David Buckley <david@davidbuckley.ca>
2023-12-11 18:58:37 -0500

    DRY it up

--- 2023/day11/day11.py
+++ 2023/day11/day11.py
@@ -5,13 +5,7 @@ from aocd import get_data
 
 data = get_data(year=2023, day=11, block=True)
 
-def print_grid(grid: Grid):
-    for y in range(grid.height):
-        for x in range(grid.width):
-            print(end=grid[y][x])
-        print()
-
-def expansion_factor(grid: Grid) -> tuple[list[bool], list[bool]]:
+def expansion_factor(grid: Grid, factor=2) -> tuple[list[int], list[int]]:
     empty_row = [True]*grid.height
     empty_column = [True]*grid.width
 
@@ -20,7 +14,10 @@ def expansion_factor(grid: Grid) -> tuple[list[bool], list[bool]]:
             empty_column[x] &= grid[y][x] == '.'
             empty_row[y] &= grid[y][x] == '.'
 
-    return list(accumulate(empty_row)), list(accumulate(empty_column))
+    return (
+        [v*(factor-1) for v in accumulate(empty_row)],
+        [v*(factor-1) for v in accumulate(empty_column)],
+    )
 
 def find_galaxies(grid: Grid) -> Iterable[tuple[int, int]]:
     for p, c in grid.row_major_with_index():
@@ -30,9 +27,9 @@ def find_galaxies(grid: Grid) -> Iterable[tuple[int, int]]:
 def manhattan_distance(p1, p2):
     return abs(p2[1]-p1[1]) + abs(p2[0]-p1[0])
 
-def part_one(data):
+def solve(data, factor):
     grid = Grid([[c for c in l] for l in data.splitlines()])
-    expand_y, expand_x = expansion_factor(grid)
+    expand_y, expand_x = expansion_factor(grid, factor)
 
     s = 0
     for (g1, g2) in combinations(find_galaxies(grid), 2):
@@ -41,21 +38,12 @@ def part_one(data):
         s += manhattan_distance(p1, p2)
 
     return s
+
+def part_one(data):
+    return solve(data, 2)
 
 def part_two(data):
-    grid = Grid([[c for c in l] for l in data.splitlines()])
-    expand_y, expand_x = expansion_factor(grid)
-    factor = 1000000
-
-    expand_y = [v*(factor-1) for v in expand_y]
-    expand_x = [v*(factor-1) for v in expand_x]
-
-    s = 0
-    for (g1, g2) in combinations(find_galaxies(grid), 2):
-        p1 = g1[0]+expand_y[g1[0]], g1[1]+expand_x[g1[1]]
-        p2 = g2[0]+expand_y[g2[0]], g2[1]+expand_x[g2[1]]
-        s += manhattan_distance(p1, p2)
-    return s
+    return solve(data, 1_000_000)
 
 print(part_one(data))
 print(part_two(data))
