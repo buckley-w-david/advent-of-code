102e24f013ebfc4e6c9c10df573fd7296cfcfb55
David Buckley <david@davidbuckley.ca>
2023-12-14 01:42:13 -0500

    add initial 2023 day 14 solution

--- 2023/day14/day14.py
+++ 2023/day14/day14.py
@@ -1,24 +1,80 @@
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
-
+from typing import cast
 from aoc_utils import * # type: ignore
 from aocd import get_data
 
 data = get_data(year=2023, day=14, block=True)
+platform = [list(line) for line in data.splitlines()]
+
+def shift(platform, dir):
+    dy, dx = dir
+    height = len(platform)
+    width = len(platform[0])
+
+    left_y = 0
+    right_y = len(platform)
+    step_y = 1
+
+    left_x = 0
+    right_x = len(platform[0])
+    step_x = 1
+
+    if dir == (1, 0):
+        left_y = len(platform)-1
+        right_y = -1
+        step_y = -1
+    elif dir == (0, 1):
+        left_x = len(platform[0])-1
+        right_x = -1
+        step_x = -1
+
+    for y in range(left_y, right_y, step_y):
+        for x in range(left_x, right_x, step_x):
+            tile = platform[y][x]
+            if tile == 'O':
+                ty, tx = min(max(y+dy, 0), height-1), min(max(x+dx, 0), width-1)
+                if platform[ty][tx] != '.':
+                    continue
+                while 0 <= ty+dy < height and 0 <= tx+dx < width and platform[ty+dy][tx+dx] == '.':
+                    ty, tx = ty+dy, tx+dx
+                platform[y][x] = '.'
+                platform[ty][tx] = 'O'
+
+def north_load(platform):
+    height = len(platform)
+    load = 0
+    for y in range(len(platform)):
+        for x in range(len(platform)):
+            if platform[y][x] == 'O':
+                load += height-y
+    return load
+
+def display(platform):
+    for line in platform:
+        print(''.join(line))
+
+
+def raster(platform):
+    return '\n'.join(''.join(line) for line in platform)
+
+
+history = {}
+for cycle in range(1_000_000_000):
+    for d in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+        shift(platform, d)
+    r = raster(platform)
+    if r in history:
+        start = history[r]
+        period = cycle - start
+        skip = period * (1_000_000_000 // period) + start
+
+        while skip > 1_000_000_000:
+            skip -= period
+
+        for cycle2 in range(skip+1, 1_000_000_000):
+            for d in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+                shift(platform, d)
+        break
+    else:
+        history[r] = cycle
+
+print(north_load(platform))
