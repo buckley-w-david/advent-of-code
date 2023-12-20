f9863356f54a27f94061b3f35b2af07a887a9660
David Buckley <david@davidbuckley.ca>
2023-12-20 11:14:04 -0500

    add 2023 day 20 part 1

--- 2023/day20/day20.py
+++ 2023/day20/day20.py
@@ -1,24 +1,96 @@
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
-
+from collections import defaultdict, deque
+import re
 from aoc_utils import * # type: ignore
 from aocd import get_data
 
+from dataclasses import dataclass
+
+class Module:
+    name: str
+    destinations: list[str]
+
+    def pulse(self, source: str, signal: bool) -> list[tuple[bool, str]]:
+        ...
+
+    def send(self, signal) -> list[tuple[str, str, bool]]:
+        return [(self.name, dst, signal) for dst in self.destinations]
+
+
+@dataclass
+class FlipFlop(Module):
+    name: str
+    destinations: list[str]
+    state: bool = False
+
+    def pulse(self, source: str, signal: bool) -> list[tuple[str, str, bool]]:
+        if not signal:
+            self.state = not self.state
+            return self.send(self.state)
+        return []
+
+@dataclass
+class Conjunction(Module):
+    name: str
+    inputs: dict[str, bool]
+    destinations: list[str]
+
+    def pulse(self, source: str, signal: bool) -> list[tuple[str, str, bool]]:
+        self.inputs[source] = signal
+        return self.send(not all(self.inputs.values()))
+
+@dataclass
+class Broadcast(Module):
+    name: str
+    destinations: list[str]
+
+    def pulse(self, source: str, signal: bool) -> list[tuple[str, str, bool]]:
+        return self.send(signal)
+
+BROADCASTER = "broadcaster"
+
+def parse_modules(data):
+    modules = {}
+
+    mapping = defaultdict(list)
+    for line in data.splitlines():
+        if match := re.match(r"%(.*) -> (.*)", line):
+            name, d = match.groups()
+            destinations = d.split(", ")
+            modules[name] = FlipFlop(name, destinations)
+            mapping[name].extend(destinations)
+        elif match := re.match(r"&(.*) -> (.*)", line):
+            name, d = match.groups()
+            destinations = d.split(", ")
+            modules[name] = Conjunction(name, {}, destinations)
+            mapping[name].extend(destinations)
+        elif match := re.match(r"broadcaster -> (.*)", line):
+            d = match.group(1)
+            destinations = d.split(", ")
+            modules[BROADCASTER] = Broadcast(BROADCASTER, destinations)
+            mapping[BROADCASTER].extend(destinations)
+
+    for name, module in modules.items():
+        if isinstance(module, Conjunction):
+            for (src, dsts) in mapping.items():
+                if name in dsts:
+                    module.inputs[src] = False
+    return modules
+
+def part_one(data):
+    modules = parse_modules(data)
+    pulses = deque()
+    counts = [0, 0]
+    for i in range(1000):
+        pulses.append((None, BROADCASTER, False))
+
+        while pulses:
+            src, dst, signal = pulses.popleft()
+            if dst in modules:
+                pulses.extend(modules[dst].pulse(src, signal))
+            counts[signal] += 1
+
+    return counts[0]*counts[1]
+
 data = get_data(year=2023, day=20, block=True)
+
+print(part_one(data))
