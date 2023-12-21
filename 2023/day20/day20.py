f29b1423e22a3b3c38ef0e17d61c70716f0a1d8f
David Buckley <david@davidbuckley.ca>
2023-12-20 19:09:36 -0500

    add alternate 2023 day 20 part 2 solution

--- 2023/day20/day20.py
+++ 2023/day20/day20.py
@@ -77,6 +77,15 @@ def parse_modules(data):
                     module.inputs[src] = False
     return modules
 
+def graphviz(data):
+    print("digraph G {")
+    for line in data.splitlines():
+        if line.startswith("broadcaster"):
+            print(" ", line + ";")
+        else:
+            print(" ", line[1:] + ";")
+    print("}")
+
 def part_one(data):
     modules = parse_modules(data)
     pulses = deque()
@@ -121,7 +130,57 @@ def part_two(data):
     # they all emit high signals at the LCM
     return lcm(*cycles.values())
 
+def part_two_no_eval(data):
+    modules = parse_modules(data)
+
+    # From manual inspection - rx has a single conjunction input
+    rx_input = next(module for module in modules.values() if module.outputs == ['rx'])
+
+    # This single input is fed by a series of modules
+    outputs = [ modules[name] for name in rx_input.inputs ]
+
+    # That each have a single input to a "hub"
+    hubs = { name for mod in outputs for name in mod.inputs }
+
+    # Each hub collects the outputs of a chain of FlipFlops that form a counter
+    # We can extract what number the counter is counting towrads by analysing the chain
+
+    # The BROADCASTER module connects to the first module in the chains around the hubs
+    chain_start = modules[BROADCASTER].outputs
+
+    cycles = 1
+    for start in chain_start:
+        connections = [1]
+        current = start
+        hub = (hubs & set(modules[start].outputs)).pop()
+
+        while True:
+            # Traverse the chains of modules from the start
+            for mod in modules[current].outputs:
+                # to each node that is _not_ the  hub (or BROADCASTER or start)
+                if mod != hub and mod != BROADCASTER and mod != start:
+                    current = mod
+                    # and store if it outputs to the hub
+                    connections.append(int(hub in modules[current].outputs))
+                    break
+            else:
+                break
+
+        # This structure is a binary counter that outputs at a specific number
+        # This number is encoded in what nodes in the chain connect to the hub
+        period = 0
+        for i, n in enumerate(connections):
+            period += n*2**i
+
+        # The numbers all happen to be prime
+        # so we can multiply them for LCM
+        cycles *= period
+
+    return cycles
+
 data = get_data(year=2023, day=20, block=True)
 
 print(part_one(data))
-print(part_two(data))
+print(part_two_no_eval(data))
+
+assert part_two(data) == part_two_no_eval(data)
