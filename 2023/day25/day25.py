75f0556ff97e9edc8925c5a6b4ecaa71035c2bcf
David Buckley <david@davidbuckley.ca>
2023-12-25 01:35:10 -0500

    add 2023 day 25

--- 2023/day25/day25.py
+++ 2023/day25/day25.py
@@ -1,24 +1,57 @@
-# Grid, Direction
-# Direction.NORTH,SOUTH,EAST,WEST,NE,SE,NW,SW
-# g = Grid([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-# g.width, g.height, (y, x) in g (coords), g[(y, x)], g[(y, x)] = 5
-# for item in g => iterate over items in row major order
-# g.row_major(_with_index)() => iterate over items in row major order
-# g.column_major(_with_index)() => iterate over items in column major order
-# g.apply(func) => call func with each item
-# g.map(func) => return new Grid with results of func
-# g.ray_from((y, x), direction), yields items from a starting point in a direction
-# g.around(_with_index) => What it sounds like
-
-# Graph
-# g = Graph()
-# g.add_edge(from, to, weight=something)
-# g.dijkstra(start) => Dijkstra (has `distance_to`, and `path_to` methods)
-
-# ShuntingYard
-# Expression parser with configurable precedence for operations so you can throw out (B)EDMAS (no support for brackets)
-
-from aoc_utils import * # type: ignore
+from typing import cast
+from collections import defaultdict, deque
+from aoc_utils import *
 from aocd import get_data
 
 data = get_data(year=2023, day=25, block=True)
+
+def parse_graph(data):
+    graph = defaultdict(set)
+
+    for line in data.splitlines():
+        k, vs = line.split(": ")
+        for v in vs.split():
+            graph[k].add(v)
+            graph[v].add(k)
+
+    return graph
+
+
+def part_one(data):
+    graph = parse_graph(data)
+
+    # partition graph via BFS
+    # Found a very brief description of the technique here https://patterns.eecs.berkeley.edu/?page_id=571#1_BFS
+    for start in graph:
+        history = set(start)
+        queue = cast(deque, deque([(start, 0)]))
+        levels = {}
+        while queue:
+            node, level = queue.pop()
+
+            levels[node] = level
+
+            for other_node in graph[node]:
+                if other_node in history: continue
+                history.add(other_node)
+
+                queue.appendleft((other_node, level+1))
+
+        for level in range(min(levels.values()), max(levels.values())+1):
+            p1 = set()
+            p2 = set()
+            for node in graph:
+                if levels[node] <= level:
+                    p1.add(node)
+                else:
+                    p2.add(node)
+
+            connections = 0
+            for node in p1:
+                for conn in graph[node]:
+                    connections += conn in p2
+
+            if connections == 3:
+                return len(p1)*len(p2)
+
+print(part_one(data))
