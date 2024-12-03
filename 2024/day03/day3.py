2c92894b666416e45b6e434499de7269b1b3cac3
David Buckley <david@davidbuckley.ca>
2024-12-03 00:12:40 -0500

    2024 Day 3

--- 2024/day03/day3.py
+++ 2024/day03/day3.py
@@ -4,3 +4,34 @@ from aoc_utils import *  # type: ignore
 from aocd import get_data
 
 data = get_data(year=2024, day=3, block=True)
+
+
+def part_one(data):
+    instructions = re.findall(r"mul\((\d+),(\d+)\)", data)
+    total = 0
+
+    for a, b in instructions:
+        total += int(a) * int(b)
+
+    return total
+
+
+def part_two(data):
+    instructions = re.findall(r"mul\(\d+,\d+\)|do\(\)|don't\(\)", data)
+    enable = True
+    total = 0
+
+    for instruction in instructions:
+        if instruction == "do()":
+            enable = True
+        elif instruction == "don't()":
+            enable = False
+        elif enable:
+            a, b = ints(instruction)
+            total += a * b
+
+    return total
+
+
+print(part_one(data))
+print(part_two(data))
