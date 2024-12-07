137e12e90003649d5655e2df0607f7453e911941
David Buckley <david@davidbuckley.ca>
2024-12-07 11:23:40 -0500

    add alternate 2024 day 7 implementation

--- 2024/day07/day7.py
+++ 2024/day07/day7.py
@@ -29,6 +29,8 @@ def calibration(equations, operators):
 
             for a, op in zip(values, combo):
                 value = op(value, a)
+                if value > test:
+                    break
 
             if value == test:
                 valid = True
@@ -56,5 +58,46 @@ def part_two(data):
     return calibration(equations, [operator.add, operator.mul, concat])
 
 
-print(part_one(data))
-print(part_two(data))
+# Alternate implementations that are much faster
+# I believe this is due to not having to do all the list construction and concatenation
+# that ends up happening in operator_combinations
+
+
+def valid(test, values, operators):
+    def _valid(current, values):
+        if len(values) == 0:
+            return current == test
+
+        for op in operators:
+            if _valid(op(current, values[0]), values[1:]):
+                return True
+
+        return False
+
+    return _valid(0, values)
+
+
+def part_one_alt(data):
+    equations = [ints(line) for line in data.splitlines()]
+
+    v = 0
+    for test, *values in equations:
+        if valid(test, values, [operator.add, operator.mul]):
+            v += test
+
+    return v
+
+
+def part_two_alt(data):
+    equations = [ints(line) for line in data.splitlines()]
+
+    v = 0
+    for test, *values in equations:
+        if valid(test, values, [operator.add, operator.mul, concat]):
+            v += test
+
+    return v
+
+
+print(part_one_alt(data))
+print(part_two_alt(data))
