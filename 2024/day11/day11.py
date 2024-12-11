7c4f3a732f2726e7239d2782737123ac32c18e2e
David Buckley <david@davidbuckley.ca>
2024-12-11 10:49:34 -0500

    2024 day 11

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -1,6 +1,41 @@
-import re
+from collections import defaultdict
+from math import log10
 
 from aoc_utils import *  # type: ignore
-from aocd import get_data
 
-data = get_data(year=2024, day=11, block=True)
+data = "3935565 31753 437818 7697 5 38 0 123"
+
+
+def count_rocks(rocks, iterations):
+    # This implementation ignores the part of the problem that specifies that rocks maintain their ordering through iterations
+    # This is fine because it doesn't actually have any effect on the data we're calculating (total number of rocks)
+    for _ in range(iterations):
+        nr = defaultdict(int)
+        for rock, n in rocks.items():
+            if rock == 0:
+                nr[1] += rocks[rock]
+            elif (ld := int(log10(rock)) + 1) % 2 == 0:
+                half = 10 ** (ld // 2)
+                right = rock % half
+                left = rock // half
+                nr[left] += n
+                nr[right] += n
+            else:
+                nr[rock * 2024] += rocks[rock]
+        rocks = nr
+
+    return sum(rocks.values())
+
+
+def part_one(data):
+    rocks = {d: 1 for d in ints(data)}
+    return count_rocks(rocks, 25)
+
+
+def part_two(data):
+    rocks = {d: 1 for d in ints(data)}
+    return count_rocks(rocks, 75)
+
+
+print(part_one(data))
+print(part_two(data))
