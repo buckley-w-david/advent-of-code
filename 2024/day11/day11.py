cc123f7a6bb7a364b9e556487c53baedd8d4ab37
David Buckley <david.buckley@shopify.com>
2024-12-11 10:28:19 -0500

    2024 11

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -1,6 +1,39 @@
-import re
+from collections import defaultdict
+from math import log10
 
 from aoc_utils import *  # type: ignore
-from aocd import get_data
 
-data = get_data(year=2024, day=11, block=True)
+data = "3935565 31753 437818 7697 5 38 0 123"
+
+
+def count_rocks(rocks, iterations):
+    for _ in range(iterations):
+        nr = defaultdict(int)
+        for rock, n in rocks.items():
+            if rock == 0:
+                nr[1] += rocks[rock]
+            elif (ld := int(log10(rock)) + 1) % 2 == 0:
+                half = 10 ** (ld // 2)
+                right = rock % half
+                left = rock // half
+                nr[left] += n
+                nr[right] += n
+            else:
+                nr[rock * 2024] += rocks[rock]
+        rocks = nr
+
+    return sum(rocks.values())
+
+
+def part_one(data):
+    rocks = {d: 1 for d in ints(data)}
+    return count_rocks(rocks, 25)
+
+
+def part_two(data):
+    rocks = {d: 1 for d in ints(data)}
+    return count_rocks(rocks, 75)
+
+
+print(part_one(data))
+print(part_two(data))
