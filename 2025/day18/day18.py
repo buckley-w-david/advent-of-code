6bbe146c5f2aa4ed0bd4157b5345e18a6588069e
David Buckley <david@davidbuckley.ca>
2025-12-05 18:39:23 -0500

    scaffold other 2025 days

---
+++ 2025/day18/day18.py
@@ -0,0 +1,18 @@
+with open("input.txt", "r") as f:
+    data = f.read()
+
+
+def parse(data):
+    return data
+
+
+def part_one(data):
+    data = parse(data)
+
+
+def part_two(data):
+    data = parse(data)
+
+
+print(part_one(data))
+print(part_two(data))
